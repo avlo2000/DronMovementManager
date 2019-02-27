68eb82ca02529e56b2939cf7794ea130fc854e0a
Dmytruto <dmytruto@gmail.com>
2019-02-27 21:15:23 +0200

    consider that previous speed exist

--- SatteliteSimulator/Controller/Controller.cpp
+++ SatteliteSimulator/Controller/Controller.cpp
@@ -6,28 +6,30 @@
 namespace simulator {
 
 	void Controller::SetSample(Sample &sample) {
-			this->_sample = sample;
+		this->_sample = sample;
 	}
 
 	void Controller::ControlRotation(Vector3d rotSpeed) {
-			if (this->_obj == NULL)
-				throw UnregisteredObjectException();
+		if (this->_obj == NULL)
+			throw UnregisteredObjectException();
+		Vector3d previousRotationSpeed = _obj->GetRotationSpeeds();
+		VectorXd  previosAndCurrentRotationSpeed;
+		previosAndCurrentRotationSpeed << previousRotationSpeed, rotSpeed;
+		MatrixXd rotSpeedMatrix = previosAndCurrentRotationSpeed;
+		MatrixXd predictionMatrix = this->_neuralNetwork.Predict(rotSpeedMatrix);
+		int numberOfWheels = _obj->GetNumOfWheels();
+		for (int i = 0; i < numberOfWheels; i++) {
+			_obj->EnergyToReactionWheel(i, predictionMatrix(i, 0));
+		}
 
-			MatrixXd rotSpeedMatrix = rotSpeed;
-			MatrixXd predictionMatrix = this->_neuralNetwork.Predict(rotSpeedMatrix);
-			int numberOfWheels = _obj->GetNumOfWheels();
-			for (int i = 0; i < numberOfWheels; i++) {
-				_obj->EnergyToReactionWheel(i, predictionMatrix(i, 0));
-			}
-			
 	}
 
-		void Controller::Train(string path) {
-			Generate(RANGESTART, RANGEEND, NUMBEROFSAMPLES);
-			this->_neuralNetwork.SetHiddenLayers(NUMBEROFHIDDENLAYERS, HIDDENLAYERSLENGTH, this->_obj->GetNumOfWheels());
-			this->_neuralNetwork.SetLearningRate(LEARNINGRATE);
-			this->_neuralNetwork.Train(this->_sample, BATCHSIZE, EPOCH);
-			this->_neuralNetwork.Save(path);
+	void Controller::Train(string path) {
+		Generate(RANGESTART, RANGEEND, NUMBEROFSAMPLES);
+		this->_neuralNetwork.SetHiddenLayers(NUMBEROFHIDDENLAYERS, HIDDENLAYERSLENGTH, this->_obj->GetNumOfWheels());
+		this->_neuralNetwork.SetLearningRate(LEARNINGRATE);
+		this->_neuralNetwork.Train(this->_sample, BATCHSIZE, EPOCH);
+		this->_neuralNetwork.Save(path);
 	}
 
 	void Controller::LoadNetwork(string fileName) {
@@ -37,48 +39,53 @@ namespace simulator {
 	}
 
 	void Controller::Generate(double rangeStart, double rangeEnd, int numberOfSamples) {
-			if (this->_obj == NULL)
-				throw UnregisteredObjectException();
+		if (this->_obj == NULL)
+			throw UnregisteredObjectException();
 
-			vector<double> firstNeuronInput;
-			int wheelSize = _obj->GetNumOfWheels();
-			int numberOfRanges = 5 * log10(numberOfSamples);
-			double step = (double)(rangeEnd - rangeStart) / numberOfRanges;
-			for (int i = 0, currentSize = 0; i < numberOfRanges; i++) {
-				int numberOfPointsInThisRange = (i == numberOfRanges - 1) ? numberOfSamples / numberOfRanges + numberOfSamples % numberOfRanges : numberOfSamples / numberOfRanges;
-				for (int j = 0; j < numberOfPointsInThisRange; j++, currentSize++) {
-					firstNeuronInput.push_back(rangeStart + (rand() / (RAND_MAX + 1.0) *(rangeStart + step - rangeStart)));
+		vector<double> firstNeuronInput;
+		int wheelSize = _obj->GetNumOfWheels();
+		int previousAndCurentWheelSize = wheelSize * 2;
+		int numberOfRanges = numberOfSamples;
+		double step = (double)(rangeEnd - rangeStart) / numberOfRanges;
+		for (int i = 0, currentSize = 0; i < numberOfRanges; i++) {
+			int numberOfPointsInThisRange = (i == numberOfRanges - 1) ? numberOfSamples / numberOfRanges + numberOfSamples % numberOfRanges : numberOfSamples / numberOfRanges;
+			for (int j = 0; j < numberOfPointsInThisRange; j++, currentSize++) {
+				firstNeuronInput.push_back(rangeStart + (rand() / (RAND_MAX + 1.0) *(rangeStart + step - rangeStart)));
+			}
+			rangeStart += step;
+		}
+		vector<vector<double>> vectorMatrix;
+		for (int i = 0; i < previousAndCurentWheelSize; i++) {
+			vector<double> NeuronInput(firstNeuronInput);
+			vectorMatrix.push_back(NeuronInput);
+		}
+
+		for (int size = firstNeuronInput.size(); 0 < size; size--) {
+			Satellite sattelite = *(_obj);
+
+			VectorXd inputVector(wheelSize);
+			Vector3d previousRotationVector;
+			for (int i = 0; i < previousAndCurentWheelSize; i++) {
+				if (i == wheelSize) {
+					previousRotationVector = sattelite.GetRotationSpeeds();
 				}
-				rangeStart += step;
+				int randIndex = rand() % size;
+				sattelite.EnergyToReactionWheel(i, vectorMatrix.at(i).at(randIndex));
+				if (i >= wheelSize)
+					inputVector(i - wheelSize) = vectorMatrix.at(i - wheelSize).at(randIndex);
+				vectorMatrix.at(i).erase(vectorMatrix.at(i).begin() + randIndex);
 			}
-			vector<vector<double>> vectorMatrix;
-			for (int i = 0; i < wheelSize; i++) {
-				vector<double> NeuronInput(firstNeuronInput);
-				vectorMatrix.push_back(NeuronInput);
-			}
 
-			for (int size = firstNeuronInput.size(); 0 < size; size--) {
-				Satellite sattelite = *(_obj);
+			this->_sample.AddEnergy(inputVector); //power
+			Vector3d rotationVector = sattelite.GetRotationSpeeds();
+			VectorXd previousAndCurrentRotation;
+			previousAndCurrentRotation << previousRotationVector, rotationVector;
+			VectorXd excpectedOutput = this->_sample.ConvertVector3dToXd(rotationVector);
 
-				VectorXd inputVector(wheelSize);
-				for (int i = 0; i < wheelSize; i++) {
-					int randIndex = rand() % size;
-					sattelite.EnergyToReactionWheel(i, vectorMatrix.at(i).at(randIndex));
-					inputVector(i) = vectorMatrix.at(i).at(randIndex);
-					vectorMatrix.at(i).erase(vectorMatrix.at(i).begin() + randIndex);
-				}
-				sattelite.Rotate(1.0);
+			this->_sample.AddRotSpeed(excpectedOutput);
 
-				this->_sample.AddEnergy(inputVector); //power
-
-				Vector3d rotationVector = sattelite.GetRotationSpeeds();
-
-				VectorXd excpectedOutput = this->_sample.ConvertVector3dToXd(rotationVector);
-
-				this->_sample.AddRotSpeed(excpectedOutput);
-
-			}
+		}
 
 	}
-		
+
 }
